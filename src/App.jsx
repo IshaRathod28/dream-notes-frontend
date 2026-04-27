a9253ce5dbf129e7b75f90f2baf3cf68a6a5d85c
Isha Rathod <isha.rathod@reddoorz.com>
2026-04-27 15:38:14 +0530

    Added changes to include the talwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import NotebookList from "./components/NotebookList";
+import CreateNotebook from "./components/CreateNotebook";
+
 function App() {
   return (
     <div>
       <h1>Dream Notes</h1>
+
+      <CreateNotebook />
+
+      <NotebookList />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
